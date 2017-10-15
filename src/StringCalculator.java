83c374aa4f871cc25960c8f05e6f0b5e0528f26f
sbaldurs <solvib11@ru.is>
2017-10-15 21:05:28 +0000

    The function Add now throws an exception if there are negative numbers in the input string

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,42 +2,65 @@
 public class StringCalculator {
 	public static int Add(String numbers) {
 		int sum = 0;
+		int count = 0;
+		boolean inputCorrect = true;
+		String comment = "Negatives not allowed: ";
 		
 		if(numbers.length() == 0) {
 			return 0;
+		}
+		
+		for(int i = 0; i < numbers.length(); i++) {
+			if(numbers.charAt(i) == '-') {
+				if(count > 0) {
+					comment += ",";
+				}
+				count++;
+				comment += numbers.substring(i, i+2);
+				inputCorrect = false;
+			}
+		}
+		
+		if(!inputCorrect) {
+			throw new java.lang.IllegalArgumentException(comment);
 		}
 		
 		for(int i = 0; i < numbers.length(); i += 2) {
 			if(numbers.charAt(i) == 'n') {
 				i++;
 			}
-			sum += Character.getNumericValue(numbers.charAt(i));
-					
+			sum += Character.getNumericValue(numbers.charAt(i));		
 		}
 		
 		return sum;
 	}
 	
 	public static void main(String[] args) {
-		String s1 = "";
+		/*String s1 = "";
 		String s2 = "1";
 		String s3 = "1,2";
 		String s4 = "1,2,3,4,5";
 		String s5 = "1,2,3,4,5,6,7,8,9,10";
-		String s6 = "1\n2,3";
+		String s6 = "1\n2,3";*/
+		String s7 = "-1,2";
+		//String s8 = "2,-4,3,-5";
 		
-		int sum1 = Add(s1);
+		/*int sum1 = Add(s1);
 		int sum2 = Add(s2);
 		int sum3 = Add(s3);
 		int sum4 = Add(s4);
 		int sum5 = Add(s5);
-		int sum6 = Add(s6);
+		int sum6 = Add(s6);*/
+		int sum7 = Add(s7);
+		//int sum8 = Add(s8);
 		
-		System.out.println(sum1);
+		/*System.out.println(sum1);
 		System.out.println(sum2);
 		System.out.println(sum3);
 		System.out.println(sum4);
 		System.out.println(sum5);
-		System.out.println(sum6);
+		System.out.println(sum6);*/
+		System.out.println(sum7);
+		//System.out.println(sum8);
 	} 
 }
